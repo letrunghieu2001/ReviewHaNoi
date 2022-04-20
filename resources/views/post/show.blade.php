16b6f00405bd2ec305f2404d49275f31271401f3
Trung Hieu Le <letrunghieu2001@gmail.com>
2022-04-20 16:10:22 +0700

    fix a few things

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -23,7 +23,7 @@
           <a href='{{ $post->link}}'>Xem trang web</a>
         </div>
         <div class="link">
-          <a href='{{ $post->map}}'>Bản đồ</a>
+          <a href='{{ $post->link}}'>Bản đồ</a>
         </div>
       </div>
      
