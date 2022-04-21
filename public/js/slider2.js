c30deee5a28aba61becd9498f973b3c646e2f7de
Lockig <minhntt2001hn@gmail.com>
2022-04-21 14:31:45 +0700

    slider doc

--- public/js/slider2.js
+++ public/js/slider2.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
   $('.image-slider-2').slick({
-    slidesToShow:3,
+    slidesToShow:2,
     slidesToScroll:2,
     infinite:true,
-    // autoplay:true,
-    // autoplaySpeed:2000,
+    autoplay:true,
+    autoplaySpeed:2000,
     draggable:false,
     arrows:true,
     vertical:true,
-    prevArrow:`<button type='button' class='slick-prev slick-arrow pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>`,
-    nextArrow:`<button type='button' class='slick-next slick-arrow pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>`,
+    prevArrow:`<button type='button' class='slick-prev slick-arrow pull-left'><i class="fa fa-angle-up"></i></button>`,
+    nextArrow:`<button type='button' class='slick-next slick-arrow pull-right'><i class='fa fa-angle-down' aria-hidden='true'></i></button>`,
     // dots:true,
     responsive:[
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
 
         }
       },
@@ -24,7 +24,8 @@ $(document).ready(function(){
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1,
+          vertical:false
         }
       },
       {
