a0e441d12fbfe915fc30644d6ddbe9941e913475
Trung Hieu Le <letrunghieu2001@gmail.com>
2022-04-23 22:16:43 +0700

    fix timezone

--- resources/views/layouts/user_edit/_header.blade.php
+++ resources/views/layouts/user_edit/_header.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="{{ asset('assets/img/logo.png') }}">
    <!-- CSS -->
    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
